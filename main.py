ad0c59595c50eebe42e2b38fea132b71f913abf9
elena <elena.ahmed1416@gmail.com>
2022-10-09 17:35:46 -0500

    Added relavant main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
 import gi
 
+from VidLibWindow import VidLibWindow
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-win = Gtk.Window()
+win = VidLibWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
-
-
