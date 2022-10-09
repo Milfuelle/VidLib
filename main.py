1ae83f61e8f6e6f0fc2415c4ad47fad0755ba36f
elena <elena.ahmed1416@gmail.com>
2022-10-09 17:34:55 -0500

    Made class for VidLib Window

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 import gi
-gi.require_version('Gtk', '3.0')
+
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-print(f'{Gtk.MAJOR_VERSION}.{Gtk.MINOR_VERSION}.{Gtk.MICRO_VERSION}')
-
-window = Gtk.Window(title="Hello World")
-window.show()
-window.connect("destroy", Gtk.main_quit)
+win = Gtk.Window()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
 Gtk.main()
+
+
